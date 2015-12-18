08be1993fb16a6d7d6bec65378f722b6b2d29d48
JamieKalloe <Jamie@Kalloe.net>
2015-12-18 17:30:23 +0100

    added mediaplayer (sound) to button

--- app/src/main/java/net/kalloe/ggbutton/MainActivity.java
+++ app/src/main/java/net/kalloe/ggbutton/MainActivity.java
@@ -1,16 +1,31 @@
 package net.kalloe.ggbutton;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Variables
+    private MediaPlayer ggSound;
+    private ImageButton ggButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeView(R.id.ibGG);
+    }
+
+    private void initializeView(int...viewIds) {
+        this.ggButton = (ImageButton) findViewById(viewIds[0]);
+        this.ggButton.setOnClickListener(this);
+        this.ggSound = MediaPlayer.create(this, R.raw.ggsound);
     }
 
     @Override
@@ -31,5 +46,22 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ibGG:
+                try {
+                    ggSound.start();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(this, "Unable to claim victory", Toast.LENGTH_LONG).show();
+                }
+                break;
+
+            default:
+                break;
+        }
     }
 }
