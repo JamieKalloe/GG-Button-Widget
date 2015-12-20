38e32341e80e81babe09999fde13e65d88e148c5
JamieKalloe <Jamie@Kalloe.net>
2015-12-20 14:00:59 +0100

    cleaned up code

--- app/src/main/java/net/kalloe/ggbutton/MainActivity.java
+++ app/src/main/java/net/kalloe/ggbutton/MainActivity.java
@@ -3,8 +3,6 @@ package net.kalloe.ggbutton;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -32,33 +30,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             this.ggSound = MediaPlayer.create(this, R.raw.ggsound);
             AdView adBanner = (AdView) findViewById(viewIds[1]);
             AdRequest adRequest = new AdRequest.Builder().build();
-//            AdRequest adRequest = new AdRequest.Builder()
-//                    .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)        // All emulators
-//                    .addTestDevice("AC98C820A50B4AD8A2106EDE96FB87D4")  // An example device ID
-//                    .build();
             adBanner.loadAd(adRequest);
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                break;
-
-            default:
-                break;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
